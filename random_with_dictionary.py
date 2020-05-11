2c16e203eefe3c4391fb704754cb61466d981a94
AljosaZ <znidaric.aljosa@gmail.com>
2020-05-11 14:08:54 +0200

    added a list of secret numbers and the solution to the dictionary. Print only best three results.

--- random_with_dictionary.py
+++ random_with_dictionary.py
@@ -6,26 +6,27 @@ from operator import itemgetter
 secret = random.randint(1, 30)
 attempts = 0
 user = input("Please enter your name: ")
-{"attempts": attempts, "date": datetime.datetime.now(), "user": user}
 
 
 with open("score_list.txt", "r") as score_file:
     score_list = json.loads(score_file.read())
     ordered_score_list = sorted(score_list, key=itemgetter("attempts"))
 
-    for score_dict in ordered_score_list:
-        print(f'The user {score_dict["user"]} has needed {score_dict["attempts"]} attempts, date: {score_dict.get("date")}')
+    for score_dict in ordered_score_list[:3]:
+        print(f'The user {score_dict["user"]} has needed {score_dict["attempts"]} attempts, the wrong guesses were {score_dict["wrong_guesses"]}, the solution was number {score_dict["solution"]} date: {score_dict.get("date")}')
+
+wrong_guesses_list = []
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
 
     if guess == secret:
-        print("You've guessed it - congratulations! It's number " + str(secret))
+        print("You've guessed it - congratulations! It's number " + str(secret) + ", your previoues guesses were " + str(wrong_guesses_list))
         print("Attempts needed: " + str(attempts))
 
         score_list.append(
-            {"attempts": attempts, "date": str(datetime.datetime.now()), "user": user}
+            {"attempts": attempts, "date": str(datetime.datetime.now()), "user": user, "wrong_guesses": wrong_guesses_list, "solution": secret}
         )
 
         with open("score_list.txt", "w") as score_file:
@@ -36,3 +37,5 @@ while True:
         print("Your guess is not correct... try something smaller")
     elif guess < secret:
         print("Your guess is not correct... try something bigger")
+
+    wrong_guesses_list.append(guess)
